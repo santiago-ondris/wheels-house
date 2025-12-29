23b78368da9f6db2694605f5a7037fa0287685b1
tonimondejar <antonio.mondejar@mi.unc.edu.ar>
2025-12-29 17:47:47 -0300

    user table

--- apps/api/src/database/schema.ts
+++ apps/api/src/database/schema.ts
@@ -1,1 +1,19 @@
 // Schema de la base de datos - definir tablas aca
+import { serial, text, integer, date, boolean, unique, pgTable } from "drizzle-orm/pg-core";
+
+
+export const user = pgTable("user",{
+    userId: serial("userId").primaryKey(),
+    username: text("username").notNull(), 
+    firstName: text("firstName").notNull(),
+    lastName: text("lastName").notNull(),
+    email: text("email").notNull(),
+    hashedPassword: text("hashedPassword").notNull(),
+    createdDate: date("createdDate").notNull(),
+    // for the future:
+    // verificationCode: integer("verificationCode").notNull(),
+    // verified: boolean("verified"),
+    // restorePasswordCode: integer("restorePasswordCode"),
+}, (t) =>[
+    unique().on(t.username)
+]);
