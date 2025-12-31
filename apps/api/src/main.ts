a63563eba4bef17d11894ddcfce32a5358ab5c9b
tonimondejar <antonio.mondejar@mi.unc.edu.ar>
2025-12-31 16:18:44 -0300

    validators

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  // Add this global pipe
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true, // <--- IMPORTANT: This enables class-transformer to work
+    whitelist: true, // Removes properties not in the DTO
+  }));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
