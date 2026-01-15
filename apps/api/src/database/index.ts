68e98cfefd514e6ae0fd09694a6ba0baecf1b5d6
Santiago Ondris <santiagonicolas2001@gmail.com>
2026-01-14 22:00:27 -0300

    fix en index.ts para que ande la db el railway

--- apps/api/src/database/index.ts
+++ apps/api/src/database/index.ts
@@ -4,11 +4,7 @@ import { Pool } from "pg";
 import * as schema from "./schema";
 
 const pool = new Pool({
-  host: process.env.DATABASE_HOST || "localhost",
-  port: Number(process.env.DATABASE_PORT) || 5432,
-  user: process.env.DATABASE_USER || "postgres",
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME || "wheels_db",
+  connectionString: process.env.DATABASE_URL,
 });
 
 export const db = drizzle(pool, {
